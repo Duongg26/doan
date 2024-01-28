ad20add27e0c806ecaa9b24caade2a0bef48019f
Duongg26 <dlevanhp@gmail.com>
2024-01-28 21:33:38 +0700

    first commit

---
+++ action/logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+unset($_SESSION['u_id']);
+if(isset($_SESSION['pro_id'])) {
+    $x = $_SESSION['pro_id'];
+    header("Location: http://localhost/doan/product.php?pro_id=$x");
+    unset($_SESSION['pro_id']);
+    session_destroy();
+} else {
+    header("Location: http://localhost/doan/");
+}
+
+?>
